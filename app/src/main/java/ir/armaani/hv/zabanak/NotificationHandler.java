bfb52752ae0cd36e77da94988996e4fc507e33ae
Siamak <siamak20068@yahoo.com>
2016-07-19 11:59:38 +0430

    PushCompleted 95/04/29 12:00

--- app/src/main/java/ir/armaani/hv/zabanak/NotificationHandler.java
+++ app/src/main/java/ir/armaani/hv/zabanak/NotificationHandler.java
@@ -8,12 +8,15 @@ import android.support.v7.app.NotificationCompat;
 import com.onesignal.NotificationExtenderService;
 import com.onesignal.OSNotificationPayload;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 import ir.armaani.hv.zabanak.activities.MessageActivity;
+import ir.armaani.hv.zabanak.activities.StoreActivity;
+import ir.armaani.hv.zabanak.models.Series;
 
 /**
  * Created by Siamak on 19/07/2016.
@@ -35,14 +38,22 @@ public class NotificationHandler extends NotificationExtenderService {
                 case 1:
                     String messageTitle = notificationPayload.getString("messageTitle");
                     String messageContent = notificationPayload.getString("messageContent");
-                    showNotification(messageTitle , messageContent , MessageActivity.class , null);
+                    showNotification(messageTitle , messageContent , MessageActivity.class );
                     break;
                 case 2:
-
+                    JSONArray series = notificationPayload.getJSONArray("series");
+                    Integer newSeriesCount = 0;
+                    for (int i = 0 ; i < series.length() ; i++) {
+                        Integer seriesId = series.getInt(i);
+                        Long count = Series.count(Series.class , "SERVER_ID = ?" , new String[] {seriesId.toString()});
+                        if (count < 1 )
+                            newSeriesCount++;
+                    }
+                    if (newSeriesCount > 0) {
+                        showNotification("مجموعه های جدید آموزشی در زبانک" , newSeriesCount + " مجموعه جدید منتظر شماست" , StoreActivity.class );
+                    }
                     break;
             }
-
-
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -51,7 +62,7 @@ public class NotificationHandler extends NotificationExtenderService {
         return true;
     }
 
-    protected void showNotification(String title , String content , Class<?> target , JSONObject data) {
+    protected void showNotification(String title , String content , Class<?> target ) {
         int mNotificationId =  getId();
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
@@ -64,8 +75,8 @@ public class NotificationHandler extends NotificationExtenderService {
         Intent resultIntent = new Intent(this,target);
         resultIntent.putExtra("title" , title);
         resultIntent.putExtra("content" , content);
-        if (data != null)
-            resultIntent.putExtra("data" , data.toString());
+        //if (data != null)
+        //    resultIntent.putExtra("data" , data.toString());
 
         PendingIntent resultPendingIntent =
                 PendingIntent.getActivity(
